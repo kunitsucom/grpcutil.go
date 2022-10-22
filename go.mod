b7633e57513b15f4bbc8823e1bcffad46a1dcca1
ginokent <29125616+ginokent@users.noreply.github.com>
2022-10-22 09:36:57 +0900

    feat: add runtimez.RegisterHandlers

--- go.mod
+++ go.mod
@@ -3,16 +3,15 @@ module github.com/kunitsuinc/grpcutil.go
 go 1.19
 
 require (
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.11.3
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.12.0
 	golang.org/x/net v0.0.0-20221004154528-8021a29435af
-	google.golang.org/grpc v1.50.0
+	google.golang.org/grpc v1.50.1
 )
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
 	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20220930163606-c98284e70a91 // indirect
+	golang.org/x/text v0.3.8 // indirect
+	google.golang.org/genproto v0.0.0-20221014213838-99cd37c6964a // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
--- grpc-gateway/v2/runtimez/runtimez.go
+++ grpc-gateway/v2/runtimez/runtimez.go
@@ -10,9 +10,22 @@ import (
 	"google.golang.org/grpc"
 )
 
-type registerHandler = func(ctx context.Context, mux *gw_runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error)
+type registerHandler = func(ctx context.Context, mux *gw_runtime.ServeMux, conn *grpc.ClientConn) (err error)
 
-func RegisterHandlersFromEndpoint(ctx context.Context, mux *gw_runtime.ServeMux, endpoint string, opts []grpc.DialOption, handlers ...registerHandler) error {
+func RegisterHandlers(ctx context.Context, mux *gw_runtime.ServeMux, conn *grpc.ClientConn, handlers ...registerHandler) error {
+	for _, handler := range handlers {
+		if err := handler(ctx, mux, conn); err != nil {
+			funcName := runtime.FuncForPC(reflect.ValueOf(handler).Pointer()).Name()
+			return fmt.Errorf("%s: %w", funcName, err)
+		}
+	}
+
+	return nil
+}
+
+type registerHandlerFromEndpoint = func(ctx context.Context, mux *gw_runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error)
+
+func RegisterHandlersFromEndpoint(ctx context.Context, mux *gw_runtime.ServeMux, endpoint string, opts []grpc.DialOption, handlers ...registerHandlerFromEndpoint) error {
 	for _, handler := range handlers {
 		if err := handler(ctx, mux, endpoint, opts); err != nil {
 			funcName := runtime.FuncForPC(reflect.ValueOf(handler).Pointer()).Name()
