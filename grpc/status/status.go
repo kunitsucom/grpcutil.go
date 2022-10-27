a8e53ab2bcdc785a60362ce8a50dec5e34803a25
ginokent <29125616+ginokent@users.noreply.github.com>
2022-10-28 03:49:04 +0900

    fix: embed *status.Status in *statusz.Status

--- grpc/status/status.go
+++ grpc/status/status.go
@@ -3,7 +3,9 @@ package statusz
 import (
 	"fmt"
 
+	"github.com/golang/protobuf/proto"
 	"golang.org/x/xerrors"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
@@ -11,46 +13,58 @@ import (
 //
 // nolint: errname
 type Status struct {
-	grpcStatus *status.Status
-	err        error
-	frame      xerrors.Frame
+	*status.Status
+	err   error
+	frame xerrors.Frame
 }
 
 // New returns *github.com/kunitsuinc/grpcutil.go/grpc/status.Status.
-func New(grpcStatus *status.Status, err error) *Status {
+func New(c codes.Code, msg string, err error) *Status {
 	return &Status{
-		grpcStatus: grpcStatus,
-		err:        err,
-		frame:      xerrors.Caller(1),
+		Status: status.New(c, msg),
+		err:    err,
+		frame:  xerrors.Caller(1),
 	}
 }
 
-func NewWithCallerSkip(grpcStatus *status.Status, err error, skip int) *Status {
+// New returns *github.com/kunitsuinc/grpcutil.go/grpc/status.Status with caller skip.
+func NewWithCallerSkip(c codes.Code, msg string, err error, skip int) *Status {
 	return &Status{
-		grpcStatus: grpcStatus,
-		err:        err,
-		frame:      xerrors.Caller(skip),
+		Status: status.New(c, msg),
+		err:    err,
+		frame:  xerrors.Caller(skip),
 	}
 }
 
-func (e *Status) GRPCStatus() *status.Status {
-	return e.grpcStatus
+func (sz *Status) GRPCStatus() *status.Status {
+	return sz.Status
 }
 
-func (e *Status) Error() string {
-	return e.err.Error()
+func (sz *Status) WithDetails(details ...proto.Message) (*Status, error) {
+	st, err := sz.Status.WithDetails(details...)
+	if err != nil {
+		return nil, fmt.Errorf("(*status.Status).WithDetails: %w", err)
+	}
+
+	sz.Status = st
+
+	return sz, nil
 }
 
-func (e *Status) Unwrap() error {
-	return e.err
+func (sz *Status) Error() string {
+	return sz.err.Error()
 }
 
-func (e *Status) Format(s fmt.State, v rune) {
-	xerrors.FormatError(e, s, v)
+func (sz *Status) Unwrap() error {
+	return sz.err
 }
 
-func (e *Status) FormatError(p xerrors.Printer) error {
-	p.Print(e.Error())
-	e.frame.Format(p)
-	return e.err
+func (sz *Status) Format(s fmt.State, v rune) {
+	xerrors.FormatError(sz, s, v)
+}
+
+func (sz *Status) FormatError(p xerrors.Printer) error {
+	p.Print(sz.Error())
+	sz.frame.Format(p)
+	return sz.err
 }
