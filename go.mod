d3daf9e5ec28ef5d818b10477e207fb2746d797e
ginokent <29125616+ginokent@users.noreply.github.com>
2022-11-06 04:27:23 +0900

    feat: add grpc-ecosystem/grpc-gateway package

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/golang/protobuf v1.5.2
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.12.0
+	github.com/kunitsuinc/util.go v0.0.18
 	golang.org/x/net v0.0.0-20221004154528-8021a29435af
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 	google.golang.org/grpc v1.50.1
--- grpc/grpc.go
+++ grpc/grpc.go
@@ -9,14 +9,14 @@ import (
 	"google.golang.org/grpc"
 )
 
-func GRPCHandler(grpcServer *grpc.Server, handler http.Handler, http2Server *http2.Server) http.Handler {
+func GRPCHandler(grpcServer *grpc.Server, grpcGatewayHandler http.Handler, http2Server *http2.Server) http.Handler {
 	return h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// NOTE: cf. https://github.com/grpc/grpc-go/issues/555#issuecomment-443293451
 		// NOTE: cf. https://github.com/philips/grpc-gateway-example/issues/22#issuecomment-490733965
 		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
 			grpcServer.ServeHTTP(w, r)
 		} else {
-			handler.ServeHTTP(w, r)
+			grpcGatewayHandler.ServeHTTP(w, r)
 		}
 	}), http2Server)
 }
